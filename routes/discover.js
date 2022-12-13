9f08d59a5f02d805742e941ec77218e716fa855e
Ankit Prakash <prakashankit526@gmail.com>
2022-12-13 22:16:42 +0530

    added discover images api

--- routes/discover.js
+++ routes/discover.js
@@ -1,15 +1,18 @@
 const { Router } = require("express");
 const route = Router();
 const CategoryModel = require("../modules/category");
+const GalleryModel = require("../modules/gallery");
 
 
-route.get("/",(req,res)=>{
-    res.send("Inside discover.");
-});
-
-route.get("/list-category",async(req,res,next)=>{
+route.get("/:category",async(req,res,next)=>{
     try {
-        const galleryDetails = await CategoryModel.find({});
+        const category = req.params.category;
+        console.log(category);
+        if(!category){
+            res.status(400).send("Bad request");
+        }
+
+        const galleryDetails = await GalleryModel.find({category: {$in: [category]}}).limit(4);
         res.send(galleryDetails);
     } catch (error) {
         console.log(error);
